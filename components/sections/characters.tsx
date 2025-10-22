8b2d3886a8ede1694e2cf520e9c6a1e4985effea
Adlet <ksss90411@gmail.com>
2025-10-22 17:51:17 +0500

    fix

--- components/sections/characters.tsx
+++ components/sections/characters.tsx
@@ -41,34 +41,46 @@ const Characters = () => {
             </button>
           </div>
         </div>
-        <div className="xl:absolute lg:left-[8%] lg:top-1/2 lg:transform mx-[10px] sm:mx-auto lg:mx-0 mt-[30px] lg:mt-0 lg:-translate-y-1/2 max-w-[562px] py-[110px] md:py-[160px] px-[40px] lg:py-[94px] lg:px-[82px] bg-[url('/character-bg-card.png')] bg-center bg-no-repeat bg-cover">
-          <h2 className="font-camieis uppercase text-[18px] lg:text-2xl mb-[13px] lg:mb-[52px]">
-            Upgrades
-          </h2>
-          <div className="max-w-[440px] leading-[140%] space-y-[22px] text-[13px] lg:text-base">
-            <p>
-              During each stage, the player defeats enemies, from which crystals
-              drop. These crystals convert into experience points and levels.
-              For each level gained, the player earns one skill point. Upon
-              completing the stage, the player is presented with a choice of
-              three abilities for each skill point.
-            </p>
-            <p>
-              The chosen effects stack, allowing the player to combine them and
-              select the most effective build for their playstyle. Some
-              abilities can be combined to obtain enhanced versions of skills -
-              evolution. In the event of the player's death, they lose all
-              enhancements acquired during the last game cycle.Characters have
-              different weapons and unique characteristics.
-            </p>
-            <p>
-              Playing as each character requires time to master, as the
-              mechanics of each character are unique. We create diverse
-              characters to cater to different players.
-            </p>
+        <div className="xl:absolute lg:left-[1%] lg:top-1/2 lg:transform mx-[10px] sm:mx-auto lg:mx-0 mt-[30px] lg:mt-0 lg:-translate-y-1/2 ">
+          <div className="relative max-w-[562px] py-[110px] md:py-[160px] px-[40px] lg:py-[94px] lg:px-[82px]">
+            <Image
+              className="w-full h-full absolute left-0 top-0 z-[1]"
+              src={"/character-bg-card.png"}
+              alt="character-bg-card"
+              width={562}
+              height={562}
+            />
+            <div className="relative z-10">
+              <h2 className="font-camieis uppercase text-[18px] lg:text-2xl mb-[13px] lg:mb-[52px]">
+                Upgrades
+              </h2>
+              <div className="max-w-[440px] leading-[140%] space-y-[22px] text-[13px] lg:text-base">
+                <p>
+                  During each stage, the player defeats enemies, from which
+                  crystals drop. These crystals convert into experience points
+                  and levels. For each level gained, the player earns one skill
+                  point. Upon completing the stage, the player is presented with
+                  a choice of three abilities for each skill point.
+                </p>
+                <p>
+                  The chosen effects stack, allowing the player to combine them
+                  and select the most effective build for their playstyle. Some
+                  abilities can be combined to obtain enhanced versions of
+                  skills - evolution. In the event of the player's death, they
+                  lose all enhancements acquired during the last game
+                  cycle.Characters have different weapons and unique
+                  characteristics.
+                </p>
+                <p>
+                  Playing as each character requires time to master, as the
+                  mechanics of each character are unique. We create diverse
+                  characters to cater to different players.
+                </p>
+              </div>
+            </div>
           </div>
         </div>
-        <div className="lg:absolute top-1/2 xl:top-1/4 right-1/6 mx-auto mt-24 lg:mt-0 lg:mx-0 w-fit lg:w-auto">
+        <div className="lg:absolute top-1/2 xl:top-1/4 right-[8%] mx-auto mt-24 lg:mt-0 lg:mx-0 w-fit lg:w-auto px-4">
           <h4 className="font-camieis mb-[30px] text-2xl">Power-ups</h4>
           <div className="flex flex-row lg:flex-col">
             <div className="max-w-[186px] mb-11">
