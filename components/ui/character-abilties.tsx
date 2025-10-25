b039e4c675ff662202662021775f5f73cda184d2
Adlet <ksss90411@gmail.com>
2025-10-25 18:48:50 +0500

    changed abilties in charachters ui

--- components/ui/character-abilties.tsx
+++ components/ui/character-abilties.tsx
@@ -2,20 +2,20 @@ import Image from "next/image";
 
 const CharacterAbilties = () => {
   return (
-    <div>
+    <div className="px-41">
       <h4 className="font-camieis mb-[30px] text-2xl">Power-ups</h4>
-      <div className="flex flex-row lg:flex-col gap-[36px] lg:gap-0">
-        <div className="max-w-[186px] mb-11">
+      <div className="flex flex-row lg:flex-col gap-[25px] lg:gap-0">
+        <div className="mb-11">
           <Image src={"/shilt.svg"} alt="shilt" width={110} height={110} />
           <p className="mb-3 text-[18px] mt-3 font-bold">Double damage</p>
-          <p className="text-[13px] lg:text-base">
+          <p className="text-[13px] lg:text-base max-w-[185px]">
             Doubles the outgoing damage of all types of attacks
           </p>
         </div>
-        <div className="max-w-[186px]">
+        <div className="">
           <Image src={"/energy.svg"} alt="energy" width={110} height={110} />
           <p className="mb-3 text-[18px] mt-3 font-bold">Invincibility</p>
-          <p className="text-[13px] lg:text-base">
+          <p className="text-[13px] lg:text-base max-w-[185px]">
             The character ignores any incoming damage
           </p>
         </div>
